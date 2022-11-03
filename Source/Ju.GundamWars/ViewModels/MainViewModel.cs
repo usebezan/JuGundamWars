3752f761429cd52650974cb4226135bf1b1b3210
usebezan <deboraheleven2@gmail.com>
2022-11-03 21:50:03 +0900

    Add GitHub link
    GitHub をブラウザで開くボタンをタイトル バーに追加した。

--- Source/Ju.GundamWars/ViewModels/MainViewModel.cs
+++ Source/Ju.GundamWars/ViewModels/MainViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using Ju.GundamWars.Models.Services;
 using MaterialDesignThemes.Wpf;
 using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 using System;
+using System.Diagnostics;
 
 
 namespace Ju.GundamWars.ViewModels
@@ -17,6 +19,8 @@ namespace Ju.GundamWars.ViewModels
             DrawerContent = WindowService.DrawerContent;
             IsDrawerOpen = WindowService.IsDrawerOpen;
 
+            VisitGitHubCommand = new ReactiveCommand().WithSubscribe(VisitGitHub).AddTo(Disposables);
+
             WindowService.Snackbar.Enqueue("Welcome to J.U Gundamu Wars!");
         }
 
@@ -24,6 +28,12 @@ namespace Ju.GundamWars.ViewModels
         public SnackbarMessageQueue MessageQueue { get; }
         public ReadOnlyReactivePropertySlim<IEntryViewModel?> DrawerContent { get; }
         public ReactivePropertySlim<bool> IsDrawerOpen { get; }
+
+        public ReactiveCommand VisitGitHubCommand { get; }
+
+
+        private void VisitGitHub() =>
+            Process.Start(new ProcessStartInfo("cmd", $"/c start https://github.com/usebezan/JuGundamWars") { CreateNoWindow = true });
 
     }
 
