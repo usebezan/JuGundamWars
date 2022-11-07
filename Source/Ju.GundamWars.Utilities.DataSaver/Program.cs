f33bdc17b2b21a4d1cefcb2dedc7e422b8e09a53
usebezan <deboraheleven2@gmail.com>
2022-11-08 00:03:12 +0900

    Add DataSaver
    データを JSON ファイルとして出力するユーティリティを追加した。

---
+++ Source/Ju.GundamWars.Utilities.DataSaver/Program.cs
@@ -0,0 +1,26 @@
+﻿using Ju.GundamWars;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+namespace Ju.GundamWars.Utilities.DataSaver
+{
+
+    class Program
+    {
+
+        static void Main(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) =>
+                {
+                    CoreHosting.ConfigureServices(context, services);
+                    services.AddSingleton<Saver>();
+                })
+                .Build()
+                .Services
+                .GetRequiredService<Saver>()
+                .Run();
+
+    }
+
+}
---
+++ Source/Ju.GundamWars.Utilities.DataSaver/Saver.cs
@@ -0,0 +1,33 @@
+﻿using Ju.GundamWars.Models.Entities;
+using Ju.GundamWars.Models.Services;
+using System;
+using System.Text.Json;
+
+
+namespace Ju.GundamWars.Utilities.DataSaver
+{
+
+    public class Saver
+    {
+
+        public Saver(GwDbContext dbContext)
+        {
+            this.dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+
+        private readonly GwDbContext dbContext;
+
+
+        public void Run()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            File.WriteAllText("PilotAbility.json", JsonSerializer.Serialize(dbContext.Set<PilotAbility>().ToList(), options));
+            File.WriteAllText("SupportSlot.json", JsonSerializer.Serialize(dbContext.Set<SupportSlot>().ToList(), options));
+            File.WriteAllText("SupportBadge.json", JsonSerializer.Serialize(dbContext.Set<SupportBadge>().ToList(), options));
+            File.WriteAllText("Version.json", JsonSerializer.Serialize(dbContext.Versions.ToList(), options));
+        }
+
+    }
+
+}
