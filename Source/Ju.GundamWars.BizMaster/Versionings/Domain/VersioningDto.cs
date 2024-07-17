8f39e4cba38620d3645bab5cafebc5682c6569e5
usebezan <deboraheleven2@gmail.com>
2024-07-18 00:09:52 +0900

    wip

---
+++ Source/Ju.GundamWars.BizMaster/Versionings/Domain/VersioningDto.cs
@@ -0,0 +1,15 @@
+﻿using Ju.GundamWars.Commons.Domain;
+
+namespace Ju.GundamWars.BizMaster.Versionings.Domain;
+
+public class VersioningDto : IIdentify
+{
+
+    #region Primitives
+
+    public int Id { get; set; }
+    public string Version { get; set; } = string.Empty;
+
+    #endregion
+
+}
